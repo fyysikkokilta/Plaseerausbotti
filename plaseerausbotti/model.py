0a644b3db9ee5c8aeaada16cb622b20dc82fdea4
Niko Savola <niko@meetiqm.com>
2022-02-15 15:16:12 +0200

    Add function for initialising table matrices (#14)

--- plaseerausbotti/model.py
+++ plaseerausbotti/model.py
@@ -1,14 +1,16 @@
 import csv
 from dataclasses import dataclass
+from queue import LifoQueue
 from typing import List, Set
 
 import networkx as nx
 import numpy as np
 from thefuzz import fuzz
 
-@dataclass
+
+@dataclass(frozen=True, order=True)
 class Person:
-    """ Class to model a person in the sitsit.
+    """ Immutable class to model a person in the sitsit.
 
     Args:
         name: Name of the person
@@ -96,15 +98,60 @@ def _common_friends(person: Person, people: List[Person]):
     friends = []
     for p in people:
         if person.name != p.name and _is_similar(person.group, p.group):
-            friends.append((person.name, p.name))
+            friends.append(p)
     return friends
 
 
-def plot_graph(people):
+def initialise_tables(tables_text: str, people: List[Person]):
+    """ Return list of np.array with people initialised with corresponding IDs
+
+    Args:
+        tables_text: Given as, e.g., ``"60 60 60"``.
+
+    Returns:
+        tables: Tables as 2xN np.array with IDs corresponding to people
+        id_people_map: IDs mapped to people
+    """
+    table_sizes = map(int, tables_text.split())
+    tables = [np.zeros([table_size // 2, 2], dtype=int) for table_size in table_sizes]
+
+    id_people_map = dict(enumerate(people, 1))
+    people_id_map = {p: i for i, p in id_people_map.items()}
+    friends_map = {p: _common_friends(p, people) for p in people}
+
+    cluster_sizes = [len(e) for e in friends_map.values()]
+    people_clustered = [p for _, p in sorted(zip(cluster_sizes, friends_map.keys()))]
+    table_splits = np.cumsum([t.size for t in tables])[:-1] - 1
+
+    for table, people_in_table in zip(tables, np.split(people_clustered, table_splits)):
+
+        stack = LifoQueue()
+        for person in reversed(people_in_table):
+            stack.put(person)
+
+        i = 0
+        processed = set()
+        while not stack.empty():
+            person = stack.get()
+            if person not in processed:
+                table[i // 2, i % 2] = people_id_map[person]
+
+                for f in friends_map[person]:
+                    stack.put(f)
+
+                processed.add(person)
+                i += 1
+
+    return tables, id_people_map
+
+
+def plot_graph(people: List[Person]):
+    """ Return simple graph plot with groups as clusters
+    """
     # TODO make graph nicer
     g = nx.Graph()
     g.add_nodes_from([(p.name, {'group': p.group}) for p in people])
-    for friends in [_common_friends(p, people) for p in people]:
-        g.add_edges_from(friends)
+    for person, friends in zip(people, [_common_friends(p, people) for p in people]):
+        g.add_edges_from([(person.name, f.name) for f in friends])
 
     nx.draw(g, with_labels=True, font_weight='bold')
--- tests/test_read_csv.py
+++ tests/test_read_csv.py
@@ -1,4 +1,6 @@
-from plaseerausbotti.model import plot_graph
+import pytest
+
+from plaseerausbotti.model import plot_graph, initialise_tables
 
 
 def test_csv_loads(people):
@@ -7,4 +9,25 @@ def test_csv_loads(people):
 
 def test_plot_graph(people):
     plot_graph(people)
-    # plt.savefig('temp.png', dpi=300)
+
+
+@pytest.mark.parametrize(
+    "tables_text",
+    [('28'),
+     ('14 14'),
+     ('14 8 6')]
+)
+def test_initialise_tables(tables_text, people):
+    tables, _ = initialise_tables(tables_text, people)
+    assert all((table != 0).any() for table in tables)
+
+
+@pytest.mark.parametrize(
+    "tables_text",
+    [('0'),
+     ('27'),
+     ('14 8 2')]
+)
+def test_initialise_tables_fails_with_less_space(tables_text, people):
+    with pytest.raises(IndexError, match='out of bounds'):
+        assert initialise_tables(tables_text, people)
